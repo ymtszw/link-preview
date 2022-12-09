9ca5a72176bbe39f9c524eee71bb2711aedd9158
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2022-12-09 23:58:57 +0900

    feat: impl crude link previewer

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { parse } from "node-html-parser";
+
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   // MY_KV_NAMESPACE: KVNamespace;
@@ -25,6 +27,51 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("q");
+    if (query) {
+      const res = await fetch(query, { redirect: "follow" });
+      const body = await res.text();
+      const md = extractMetadata(body);
+
+      const ret = new Response(JSON.stringify(md));
+      ret.headers.set("content-type", "application/json");
+      return ret;
+    } else {
+      return new Response("Hello World!");
+    }
   },
 };
+
+type Metadata = {
+  title?: string | null;
+  description?: string | null;
+  url?: string | null;
+  image?: string | null;
+};
+
+function extractMetadata(html: string): Metadata {
+  const parsed = parse(html);
+
+  const title =
+    parsed
+      .querySelector('meta[property="og:title"]')
+      ?.getAttribute("content") || parsed.querySelector("title")?.textContent;
+
+  const description =
+    parsed
+      .querySelector('meta[property="og:description"]')
+      ?.getAttribute("content") ||
+    parsed.querySelector('meta[name="description"]')?.getAttribute("content") ||
+    title;
+
+  const url =
+    parsed.querySelector('link[rel="canonical"]')?.getAttribute("href") ||
+    parsed.querySelector('meta[property="og:url"]')?.getAttribute("content");
+
+  const image = parsed
+    .querySelector('meta[property="og:image"]')
+    ?.getAttribute("content");
+
+  return { title, description, url, image };
+}
